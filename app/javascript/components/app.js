3ea09ae56a6ade695a10ed98545b0ec7c9208b65
Craig Barber <craigb@mojotech.com>
2018-05-11 07:53:40 -0400

    chore(client): set up apollo graphql

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Logo from './logo';
 import FullPanel from './full-panel';
 import SidePanel from './side-panel';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <FullPanel />
-        <SidePanel />
-      </div>
+      <ApolloProvider client={client}>
+        <React.Fragment>
+          <FullPanel />
+          <SidePanel />
+        </React.Fragment>
+      </ApolloProvider>
     );
   }
 }
