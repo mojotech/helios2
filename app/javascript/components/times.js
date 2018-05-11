a27ee3541c77c2bf8b4782eb7a3ec0519f292610
Dylan Kirby <dylan@mojotech.com>
2018-05-11 12:48:00 -0400

    feature(client): query for locations and render city names

--- app/javascript/components/times.js
+++ app/javascript/components/times.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { sort, prop, descend } from 'ramda';
 import { SpacedRow } from './row';
 import Time from './time';
 import { spacing } from '../lib/theme';
@@ -9,12 +12,43 @@ const Wrapper = styled(SpacedRow)`
   margin-bottom: ${spacing.xxl};
 `;
 
+const LoadingMessage = () => <p>Loading...</p>;
+
+const ErrorMessage = ({ message }) => <p>Error: {message}</p>;
+
+const getLocations = gql`
+  {
+    locations {
+      id
+      cityName
+      isPrimary
+    }
+  }
+`;
+
 export const Times = () => (
   <Wrapper>
-    <Time primary={true} />
-    <Time primary={false} />
-    <Time primary={false} />
-    <Time primary={false} />
+    <Query query={getLocations}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <LoadingMessage />;
+        }
+
+        if (error) {
+          return <ErrorMessage message={error.message} />;
+        }
+
+        const locations = sort(descend(prop('isPrimary')), data.locations);
+
+        return (
+          <React.Fragment>
+            {locations.map(({ id, isPrimary, cityName }) => (
+              <Time key={id} {...{ isPrimary, cityName }} />
+            ))}
+          </React.Fragment>
+        );
+      }}
+    </Query>
   </Wrapper>
 );
 
