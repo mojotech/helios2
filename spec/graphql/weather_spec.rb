7a243cd41149e186c7d044e3bc035f5c05044890
Joe Alba <joe@joealba.com>
2018-05-11 15:56:11 -0400

    [rubocop] Resolve offenses in spec/graphql/weather_spec

--- spec/graphql/weather_spec.rb
+++ spec/graphql/weather_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 module WeatherHelpers
   def self.darksky_response
-    text = File.read(Rails.root.join("spec/fixtures/files/darksky_response_1.json"))
+    text = File.read(Rails.root.join("spec", "fixtures", "files", "darksky_response_1.json"))
     json = JSON.parse(text)
     Hashie::Mash.new(json)
   end
@@ -28,20 +28,22 @@ describe Helios2Schema do
 
   describe "a weather query" do
     # provide a query string for `result`
-    let(:query_string) { %|
-      {
-	weather(latitude: 40.7127, longitude: -74.0059) {
-	  currently {
-	    summary
-	    temperature
-	    time
-	  }
-	}
-      }
-    | }
+    let(:query_string) {
+      %|
+{
+  weather(latitude: 40.7127, longitude: -74.0059) {
+    currently {
+      summary
+      temperature
+      time
+    }
+  }
+}
+      |
+    }
 
     context "from anonymous" do
-      it "will consume a DarkSky response and return data" do 
+      it "will consume a DarkSky response and return data" do
         expect(result["data"]["weather"]["currently"]["summary"]).to eq("Partly Cloudy")
       end
     end
