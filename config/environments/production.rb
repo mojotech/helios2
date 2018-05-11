6c3cc9770d82cd9b3b12ce1815eecdac9094d5ed
Joe Alba <joe@joealba.com>
2018-05-11 15:51:42 -0400

    [rubocop] Resolve rubocop offenses for indentation in config/environments files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
-    config.webpacker.check_yarn_integrity = false
-  # Settings specified here will take precedence over those in config/application.rb.
+  config.webpacker.check_yarn_integrity = false
 
   # Code is not reloaded between requests.
   config.cache_classes = true
