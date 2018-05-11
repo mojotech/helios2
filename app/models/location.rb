37110920b91ca5ddcf02c5c2e3e433ec16bdbc60
Dylan Kirby <dylan@mojotech.com>
2018-05-11 12:39:46 -0400

    chore(server): add Location model and seeds

---
+++ app/models/location.rb
@@ -0,0 +1,6 @@
+class Location < ApplicationRecord
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :city_name, presence: true, uniqueness: true
+  validates :time_zone, presence: true
+end
