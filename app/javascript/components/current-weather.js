600db99d44287c37fa837d9f4578576c909094df
Dylan Kirby <dylan@mojotech.com>
2018-05-18 08:32:26 -0400

    render minutely weather summary

--- app/javascript/components/current-weather.js
+++ app/javascript/components/current-weather.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import CurrentTemp from './current-temp';
 import HourlyTemps from './hourly-temps';
+import MinutelyWeather from './minutely-weather';
 import { Row } from './row';
 
 const CenteredRow = styled(Row)`
@@ -10,8 +11,11 @@ const CenteredRow = styled(Row)`
 `;
 
 export default () => (
-  <CenteredRow>
-    <CurrentTemp />
-    <HourlyTemps />
-  </CenteredRow>
+  <div>
+    <CenteredRow>
+      <CurrentTemp />
+      <HourlyTemps />
+    </CenteredRow>
+    <MinutelyWeather />
+  </div>
 );
---
+++ app/javascript/components/minutely-weather.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import { weights, fontSizes, spacing } from '../lib/theme';
+import { WhiteText } from './typography';
+
+const LoadingMessage = () => <p>Loading...</p>;
+const ErrorMessage = ({ message }) => <p>Error: {message}</p>;
+
+const SummaryText = styled(WhiteText)`
+  font-size: ${fontSizes.xlarge};
+  font-weight: ${weights.light};
+  margin-top: ${spacing.xl};
+  padding: 0 100px;
+`;
+
+const getMinutelyWeather = gql`
+  {
+    primaryLocation {
+      weather {
+        minutely {
+          summary
+        }
+      }
+    }
+  }
+`;
+
+export default () => (
+  <Query query={getMinutelyWeather}>
+    {({ loading, error, data }) => {
+      if (loading) {
+        return <LoadingMessage />;
+      }
+
+      if (error) {
+        return <ErrorMessage message={error.message} />;
+      }
+
+      const { summary } = data.primaryLocation.weather.minutely;
+      return <SummaryText>{summary}</SummaryText>;
+    }}
+  </Query>
+);
