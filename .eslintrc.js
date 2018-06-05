ce89ce37f002380fa609966704c204c4c272454e
Craig Barber <craigb@mojotech.com>
2018-06-05 10:12:11 -0400

    chore: add eslint and airbnb default config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-indent": [0],
+    "import/no-named-as-default": [0],
+  },
+  "env": {
+    "browser": true,
+  }
+};
