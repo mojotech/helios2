a408cbe11e5a6bd15fbdafa5ca8c0399fb50022c
Craig Barber <craigb@mojotech.com>
2018-06-05 11:03:39 -0400

    chore: add prettier integrated with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   "parser": "babel-eslint",
-  "extends": "airbnb",
+  "extends": ["airbnb", "prettier"],
+  "plugins": ["prettier"],
   "rules": {
+    "prettier/prettier": "error",
     "react/jsx-indent": [0],
     "import/no-named-as-default": [0],
   },
