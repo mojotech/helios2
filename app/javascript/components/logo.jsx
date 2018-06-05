78e44e7cdf2b507e06ba8663d48c9fce6560db70
Craig Barber <craigb@mojotech.com>
2018-06-05 11:30:24 -0400

    chore: add missing alt property

--- app/javascript/components/logo.jsx
+++ app/javascript/components/logo.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export const Logo = () => (
   <img
     src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0NyIgaGVpZ2h0PSI0MCIgdmlld0JveD0iMCAwIDQ3IDQwIj4KICAgIDxwYXRoIGZpbGw9IiNGRkYiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTExLjUzNiAwTDAgMTkuOTk0bDExLjUzMSAxOS45ODkgMjMuMDcuMDE3IDExLjUzMi0yMEwzNC42IDBIMTEuNTM2em0xOC4xMzcgMTEuNjdoMy4zMzR2MTYuNzI4SDI3LjQ2di03LjE0N2wxLjA4NS0xLjU5Ni0uMDY0LS4wNjMtNS4zNTQgMy40NTctNS4zNTUtMy40NTUtLjA2NC4wNjYgMS4wODUgMS41OTF2Ny4xNDdoLTUuNjI1VjExLjY3aDMuNDExbDYuNTQ4IDQuNzI4IDYuNTQ3LTQuNzI4eiIgb3BhY2l0eT0iLjUiLz4KPC9zdmc+Cg=="
+    alt="helios"
     width={40}
     height={40}
   />
