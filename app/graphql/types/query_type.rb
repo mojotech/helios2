74584fd5bff5a7a1e1052f2008a03b3d55a1ff2e
Craig Barber <craigb@mojotech.com>
2018-06-15 14:19:42 -0400

    chore: linter var rename

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,7 +5,7 @@ Types::QueryType = GraphQL::ObjectType.define do
     description "Weather response from Dark Sky"
     argument :latitude, !types.Float
     argument :longitude, !types.Float
-    resolve ->(obj, args, ctx) {
+    resolve ->(_obj, args, _ctx) {
       Rails.cache.fetch('dark-sky-response', expires_in: 2.minutes) do
         ForecastIO.forecast(args[:latitude], args[:longitude])
       end
