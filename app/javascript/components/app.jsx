a232b781e4a1672f2c0e25cf9cb38ac58eb34772
Craig Barber <craigb@mojotech.com>
2018-06-21 15:54:44 -0400

    chore: connect apollo client to ActionCable websocket

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import ActionCable from 'actioncable';
+import { getMainDefinition } from 'apollo-utilities';
+import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
 import WidgetController from './widget-controller';
 
-const client = new ApolloClient({});
+const cable = ActionCable.createConsumer('/cable');
+
+const httpLink = new HttpLink({
+  uri: '/graphql',
+  credentials: 'include',
+});
+
+const webSocketLink = new ActionCableLink({ cable });
+
+const link = ApolloLink.split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  webSocketLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache({ addTypename: false }),
+});
 
 const App = () => (
   <ApolloProvider client={client}>
