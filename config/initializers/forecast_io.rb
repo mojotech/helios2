91ca15e3cdef2098c2ec2f977b47681001119bf6
Jen Kaplan <jenkaplan@Jens-MacBook-Pro.local>
2018-07-06 17:21:24 -0400

    rubocop-fix: ignore rules

--- config/initializers/forecast_io.rb
+++ config/initializers/forecast_io.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Style/MultilineIfModifier
 ForecastIO.configure do |c|
+  # rubocop:enable Style/MultilineIfModifier
   c.api_key = ENV['DARK_SKY_API_KEY'] || Rails.application.credentials.dark_sky[:api_key]
 end unless Rails.env.test?
