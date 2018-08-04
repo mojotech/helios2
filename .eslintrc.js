f473682940e6a3066add55e11932b3a183526e55
Craig Barber <craigb@mojotech.com>
2018-08-04 14:26:06 -0400

    chore: add graphql linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": ["airbnb", "prettier"],
-  "plugins": ["prettier", "import"],
-  "rules": {
-    "prettier/prettier": "error",
-    "react/jsx-indent": [0],
-    "import/no-named-as-default": [0],
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['prettier', 'import', 'graphql'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-indent': [0],
+    'import/no-named-as-default': [0],
+    'graphql/template-strings': [
+      'error',
+      {
+        env: 'apollo',
+        schemaJson: require('./app/javascript/schema.json'),
+      },
+    ],
   },
-  "env": {
-    "browser": true,
-    "jest": true,
+  env: {
+    browser: true,
+    jest: true,
   },
-  "settings": {
-    "import/resolver": {
-      "webpack": {
-        "config": "config/webpacker.yml",
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'config/webpacker.yml',
       },
     },
   },
