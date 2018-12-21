3a818643c50f2a39a2aaaaa71d4b0d3b8a4d87cf
will cohen <willcohen@wills-MacBook-Pro.local>
2018-12-21 12:20:05 -0500

    add unity script

---
+++ unity/Assets/script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+    Dictionary<string, GameObject> objects;
+
+    void Start()
+    {
+        objects = new Dictionary<string, GameObject>(){
+            {"pr", GameObject.Find("pr")},
+            {"commit", GameObject.Find("commit")},
+            {"slack", GameObject.Find("slack")}
+        };
+
+    }
+
+    void Update()
+    {
+        if(Input.GetButton("Fire1")){
+            addObject("pr");
+        }
+
+    }
+
+    public void addObject(string objectType)
+    {
+        Instantiate(objects[objectType], new Vector3(Random.Range(-0.5f, 0.5f),20,0), Quaternion.identity);
+    }
+}
