f8804723d63995d6c88294f87407c6a47242f13a
Mariana Pachon <marianapachon@Marianas-MacBook-Pro.local>
2019-05-13 15:42:07 -0400

    feat: add github and slack count updates to panel

--- app/javascript/components/widgets/numbers/panel.jsx
+++ app/javascript/components/widgets/numbers/panel.jsx
@@ -1,6 +1,110 @@
 import React from 'react';
-import Wrapper from '../../panel-wrapper';
+import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import { over, lensPath, inc } from 'ramda';
+import pluralize from 'pluralize';
+import {
+  colors,
+  weights,
+  fontSizes,
+  spacing,
+  fonts,
+  leftPanelWidth,
+} from '../../../lib/theme';
 
-export const Numbers = () => <Wrapper />;
+const LoadingMessage = () => <p>Loading...</p>;
+const ErrorMessage = ({ message }) => <p>Error: {message}</p>;
+ErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+const getEventCounts = gql`
+  {
+    events {
+      count {
+        githubPull
+        githubCommit
+        slackMessage
+      }
+    }
+  }
+`;
+
+const NumbersTitle = styled.div`
+  color: ${colors.white};
+  font-size: ${fontSizes.medium};
+`;
+
+const Count = styled.div`
+  color: ${colors.white};
+  weight: ${weights.bold};
+  font-size: ${fontSizes.huge};
+`;
+
+const CountLabel = styled.div`
+  font-size: ${fontSizes.medium};
+  color: ${props => props.color};
+`;
+
+const NumberWrapper = styled.div`
+  display: flex;
+  flex-wrap: no-wrap;
+  justify-content: space-between;
+  width: 5em;
+`;
+
+const SubscribedEvents = ({ githubPull, githubCommit, slackMessage }) => {
+  const commitText = pluralize('Commit', githubCommit);
+  const requestText = pluralize('request', githubPull);
+  const messageText = pluralize('message', slackMessage);
+
+  return (
+    <div>
+      <NumbersTitle>This week at MojoTech</NumbersTitle>
+      <NumberWrapper>
+        <div>
+          <Count>{githubCommit}</Count>
+          <CountLabel color={colors.pink}>{commitText}</CountLabel>
+        </div>
+        <div>
+          <Count>{githubPull}</Count>
+          <CountLabel color={colors.yellow}>Pull {requestText}</CountLabel>
+        </div>
+        <div>
+          <Count>{slackMessage}</Count>
+          <CountLabel color={colors.teal}>Slack {messageText}</CountLabel>
+        </div>
+      </NumberWrapper>
+    </div>
+  );
+};
+SubscribedEvents.propTypes = {
+  githubPull: PropTypes.number.isRequired,
+  githubCommit: PropTypes.number.isRequired,
+  slackMessage: PropTypes.number.isRequired,
+};
+
+const Numbers = () => (
+  <Query query={getEventCounts}>
+    {({ loading, error, data }) => {
+      if (loading) {
+        return <LoadingMessage />;
+      }
+
+      if (error) {
+        return <ErrorMessage message={error.message} />;
+      }
+
+      const { count } = data.events;
+      return (
+        <Count>
+          <SubscribedEvents {...count} />
+        </Count>
+      );
+    }}
+  </Query>
+);
 
 export default Numbers;
