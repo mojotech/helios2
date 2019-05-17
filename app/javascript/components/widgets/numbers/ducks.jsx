c695a107cf1f6e1f8afe0d0afb8828350f55109b
will cohen <willc@mojotech.com>
2019-05-17 00:36:36 -0400

    store world state in apollo cache

---
+++ app/javascript/components/widgets/numbers/ducks.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable graphql/template-strings */
+import React from 'react';
+import { withQuery } from 'react-apollo';
+import gql from 'graphql-tag';
+
+export const withLocalMutation = WrappedComponent => props => (
+  <WrappedComponent
+    {...{
+      ...props,
+      mutation: variables => {
+        // eslint-disable-next-line react/prop-types
+        props.client.writeData({
+          data: {
+            fallingBlocksState: {
+              __typename: 'FallingBlocksState',
+              ...variables,
+            },
+          },
+        });
+      },
+    }}
+  />
+);
+
+export const withLocalState = withQuery(
+  gql`
+    {
+      fallingBlocksState @client {
+        githubCommit
+        githubPull
+        slackMessage
+        world
+      }
+    }
+  `,
+  {
+    props: ({ data }) => ({ localState: data.fallingBlocksState }),
+  },
+);
--- app/javascript/components/widgets/numbers/falling-blocks.jsx
+++ app/javascript/components/widgets/numbers/falling-blocks.jsx
@@ -1,33 +1,48 @@
 import React from 'react';
 import { Engine, Render, World, Bodies } from 'matter-js';
-import { graphql } from 'react-apollo';
+import { graphql, withApollo } from 'react-apollo';
 import { pathOr, keys, compose } from 'ramda';
+import Resurrect from 'resurrect-js';
+import PropTypes from 'prop-types';
 import { getEventCounts } from './queries';
 import githubPull from '../../../../assets/images/pr.png';
 import githubCommit from '../../../../assets/images/commit.png';
 import slackMessage from '../../../../assets/images/slack.png';
+import { withLocalMutation, withLocalState } from './ducks';
 
 const blockTypes = { githubPull, githubCommit, slackMessage };
 
 class Scene extends React.Component {
+  static propTypes = {
+    mutation: PropTypes.func.isRequired,
+  };
+
   constructor(props) {
     super(props);
+    const { world, ...currentState } = this.getLocalState(props);
     this.state = {
       height: window.innerHeight,
       width: window.innerWidth,
-      ...this.getInitialCounts(),
+      ...currentState,
     };
     this.scene = React.createRef();
     this.timer = null;
+    this.res = new Resurrect({ prefix: '$', cleanup: true });
   }
 
   componentDidMount() {
     this.setupWorld();
+    const { world } = this.getLocalState(this.props);
+    if (world) {
+      const loadedWorld = this.res.resurrect(world);
+      Engine.merge(this.engine, { world: loadedWorld });
+    }
     this.addBlocks();
   }
 
   componentWillUnmount() {
     clearTimeout(this.timer);
+    this.save();
   }
 
   /**
@@ -112,6 +127,8 @@ class Scene extends React.Component {
     slackMessage: 0,
   });
 
+  getLocalState = pathOr(this.getInitialCounts(), ['localState']);
+
   getCountsFromProps = pathOr(this.getInitialCounts(), [
     'data',
     'events',
@@ -151,6 +168,31 @@ class Scene extends React.Component {
     this.timer = setTimeout(this.addBlocks, 200);
   };
 
+  save() {
+    // eslint-disable-next-line no-shadow
+    const { githubPull, githubCommit, slackMessage } = this.state;
+    this.props.mutation({
+      githubPull,
+      githubCommit,
+      slackMessage,
+      world: this.serialise(this.engine.world),
+    });
+  }
+
+  serialise(object) {
+    return this.res.stringify(object, (key, value) => {
+      // limit precision of floats
+      if (!/^#/.exec(key) && typeof value === 'number') {
+        const fixed = parseFloat(value.toFixed(3));
+
+        if (fixed === 0 && value !== 0) return value;
+
+        return fixed;
+      }
+      return value;
+    });
+  }
+
   render() {
     return (
       <div
@@ -165,4 +207,9 @@ class Scene extends React.Component {
   }
 }
 
-export default compose(graphql(getEventCounts))(Scene);
+export default compose(
+  withApollo,
+  graphql(getEventCounts),
+  withLocalState,
+  withLocalMutation,
+)(Scene);
