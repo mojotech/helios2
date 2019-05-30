2125fd62d9ecccb82106b531061a441e1469a0cc
Mariana Pachon <mariana@mojotech.com>
2019-05-30 13:40:20 -0400

    feat: define resolve aliases for absolute imports

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,5 @@
 const { environment } = require('@rails/webpacker');
+const customConfig = require('./webpack.config.custom');
 
+environment.config.merge(customConfig);
 module.exports = environment;
---
+++ config/webpack/webpack.config.custom.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@root': path.resolve(__dirname, '../..'),
+      '@app': path.resolve(__dirname, '../../app'),
+      '@javascript': path.resolve(__dirname, '../../app/javascript'),
+      '@lib': path.resolve(__dirname, '../../app/javascript/lib'),
+      '@components': path.resolve(__dirname, '../../app/javascript/components'),
+      '@widgets': path.resolve(
+        __dirname,
+        '../../app/javascript/components/widgets',
+      ),
+      '@messages': path.resolve(
+        __dirname,
+        '../../app/javascript/components/widgets/messages',
+      ),
+      '@numbers': path.resolve(
+        __dirname,
+        '../../app/javascript/components/widgets/numbers',
+      ),
+      '@weather': path.resolve(
+        __dirname,
+        '../../app/javascript/components/widgets/weather',
+      ),
+      '@hocs': path.resolve(__dirname, '../../app/javascript/components/hocs'),
+      '@assets': path.resolve(__dirname, '../../app/assets'),
+      '@images': path.resolve(__dirname, '../../app/assets/images'),
+      '@icons': path.resolve(__dirname, '../../app/assets/images/icons'),
+    },
+    extensions: ['.jsx', '.js'],
+  },
+};
