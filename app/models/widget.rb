b963dbf257e13795a69d855d8cc9977344bf5131
Mariana Pachon <mariana@mojotech.com>
2019-06-10 11:38:17 -0400

    feat: add 'next' method and tests in widget model

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -6,4 +6,8 @@ class Widget < ApplicationRecord
 
   default_scope { order(position: :asc) }
   scope :enabled, -> { where(enabled: true) }
+
+  def self.next_or_default(current_id)
+    current_id && where('id > ?', current_id).order(:position).first || first
+  end
 end
