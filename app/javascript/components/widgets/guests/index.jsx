14c79845ce1988424953ef8447c1986aaf3ed380
Craig Barber <craigb@mojotech.com>
2019-06-18 14:52:13 -0400

    chore: remove orphanned tab components

--- app/javascript/components/widgets/guests/index.jsx
+++ app/javascript/components/widgets/guests/index.jsx
@@ -1,7 +1,5 @@
-import Tab from './tab';
 import Panel from './panel';
 
 export default {
-  Tab,
   Panel,
 };
--- app/javascript/components/widgets/weather/index.jsx
+++ app/javascript/components/widgets/weather/index.jsx
@@ -1,7 +1,5 @@
-import Tab from '@weather/tab';
 import Panel from '@weather/panel';
 
 export default {
-  Tab,
   Panel,
 };
