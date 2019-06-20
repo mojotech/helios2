a90980720159d38855d981309f41ad2b8902e6ac
theroyalblah <simon@mojotech.com>
2019-06-20 11:16:44 -0400

    fix: weather panel properly changes size
    Moving the window up or down will correctly reformat the weather
    panel so that if fills the space and doesn't fall off screen.

--- app/javascript/components/panel-wrapper.jsx
+++ app/javascript/components/panel-wrapper.jsx
@@ -3,9 +3,11 @@ import { colors } from '@lib/theme';
 
 export const Wrapper = styled.div`
   background: ${colors.black};
-  height: calc(100vh - 200px);
-  padding: 100px;
-  width: calc(100vw - 200px);
+  height: calc(100vh - 100px);
+  padding-top: 100px;
+  padding-left: 100px;
+  width: calc(100vw - 100px);
+  overflow: hidden;
 `;
 
 export default Wrapper;
