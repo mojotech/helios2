b4ea3c60d08cdf430343f5d5cc1fae87fcd83891
theroyalblah <simon@mojotech.com>
2019-08-14 10:52:35 -0400

    feat: create livestream

--- app/javascript/components/widgets/livestream/panel.jsx
+++ app/javascript/components/widgets/livestream/panel.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+`;
 
 const LiveStream = () => {
-  return <>test</>;
+  return (
+    <Wrapper>
+      <iframe
+        title="livestream"
+        width={window.innerWidth}
+        height={window.innerHeight}
+        src=""
+        frameBorder="0"
+        allow="autoplay; encrypted-media; picture-in-picture"
+      />
+    </Wrapper>
+  );
 };
 
 export default LiveStream;
