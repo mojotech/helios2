0d51d806b357b561769fc0107b7b642be1bbf93c
Craig Barber <craigb@mojotech.com>
2020-01-23 14:41:00 -0500

    feat: add by_id_or_first for backend widgets collection
    I think `next` is still a good idea to continue removing state from the client but it breaks appart when trying to handle next/prev and clicking supportted for development.

--- app/graphql/types/widget_collection_type.rb
+++ app/graphql/types/widget_collection_type.rb
@@ -3,12 +3,20 @@ class Types::WidgetCollectionType < Types::BaseObject
   field :next, Types::WidgetType do
     argument :current_widget_id, Integer, required: false
   end
+  field :by_id_or_first, Types::WidgetType do
+    argument :id, Integer, required: false
+  end
 
   def enabled
-    object.enabled.available.all
+    object.enabled.available.order(:position).all
   end
 
   def next(current_widget_id: nil)
-    object.enabled.next_or_default(current_widget_id)
+    object.enabled.available.next_or_default(current_widget_id)
+  end
+
+  def by_id_or_first(id: nil)
+    scope = object.enabled.available.order(:position)
+    id && scope.find_by(id: id) || scope.first
   end
 end
