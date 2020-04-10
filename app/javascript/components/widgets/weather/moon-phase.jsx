bba91692241728a690e79ea6b53683af8d712791
Aaron Montecalvo <aaronmontecalvo@Aarons-MacBook-Pro.local>
2020-04-10 14:09:13 -0400

    refactor: sun and moon path now use arc

--- app/javascript/components/widgets/weather/moon-phase.jsx
+++ app/javascript/components/widgets/weather/moon-phase.jsx
@@ -2,43 +2,136 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { colors } from '@lib/theme';
 
-const iconRender = (posX, posY, radius, phase) => {
-  const shouldHideFront =
-    phase === 'newMoon' || phase === 'fullMoon' ? 'hidden' : 'visible';
-  const getBackEleFill =
-    phase.includes('Cres') || phase === 'fullMoon'
-      ? colors.white
-      : colors.black;
+const getRearPath = (radius, phase, radiusModifier1) => {
+  const diameter = radius * 2;
+  let path = [``];
+  switch (phase) {
+    case 'newMoon':
+    case 'fullMoon':
+      path = [``];
+      break;
+    case 'waxCres':
+    case 'wanGib':
+    case 'firstQuart':
+    case 'thirdQuart':
+      path = [
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier1} ${diameter * radiusModifier1}
+          0 0,1 0 ${diameter - radius}`,
+      ];
+      break;
+    case 'waxGib':
+    case 'wanCres':
+      path = [
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier1} ${diameter * radiusModifier1}
+          0 0,0 0 ${diameter - radius}`,
+      ];
+      break;
+    default:
+      path = [``];
+  }
+  return path.join(' ');
+};
 
-  const getFrontEleFill =
-    phase.includes('Gib') || phase.includes('Quart')
-      ? colors.white
-      : colors.black;
-  const useMask = !phase.includes('Quart');
+const getFrontPath = (radius, phase, radiusModifier1, radiusModifier2) => {
+  const diameter = radius * 2;
+  let path = [``];
+  switch (phase) {
+    case 'newMoon':
+    case 'fullMoon':
+      path = [
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier1} ${diameter * radiusModifier1}
+          0 0,0 0 ${diameter - radius}`,
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier1} ${diameter * radiusModifier1}
+          0 0,1 0 ${diameter - radius}`,
+      ];
+      break;
+    case 'waxCres':
+    case 'wanGib':
+      path = [
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier1} ${diameter * radiusModifier1}
+          0 0,0 0 ${diameter - radius}`,
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier2} ${diameter * radiusModifier1}
+          0 0,1 0 ${diameter - radius}`,
+      ];
+      break;
+    case 'firstQuart':
+    case 'thirdQuart':
+      path = [
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier1} ${diameter * radiusModifier1}
+          0 0,0 0 ${diameter - radius}`,
+      ];
+      break;
+    case 'waxGib':
+    case 'wanCres':
+      path = [
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier1} ${diameter * radiusModifier1}
+          0 0,1 0 ${diameter - radius}`,
+        `M 0 -${radius}`,
+        `A ${diameter * radiusModifier2} ${diameter * radiusModifier1}
+          0 0,0 0 ${diameter - radius}`,
+      ];
+      break;
+    default:
+      path = [``];
+      break;
+  }
+  return path.join(' ');
+};
+
+const iconRender = (posX, posY, radius, phase, arcPath, arcAnimation) => {
+  const radiusInt = parseInt(radius, 10);
+  const radiusModifier1 = 0.4;
+  const radiusModifier2 = 0.15;
+  const rearMoonPath = getRearPath(
+    radiusInt,
+    phase,
+    radiusModifier1,
+    radiusModifier2,
+  );
+  const frontMoonPath = getFrontPath(
+    radiusInt,
+    phase,
+    radiusModifier1,
+    radiusModifier2,
+  );
+  const rearMoonColor = phase.match(/^(newMoon|waxCres|firstQuart|wanCres)$/)
+    ? colors.white
+    : colors.black;
+  const frontMoonColor =
+    rearMoonColor === colors.white ? colors.black : colors.white;
   return (
     <g>
-      <circle
-        cx={posX}
-        cy={posY}
-        r={radius}
-        fill={getBackEleFill}
+      <path
+        d={rearMoonPath}
+        fill={rearMoonColor}
         filter="url(#moonShadow)"
+        style={{
+          offsetPath: `path('${arcPath}')`,
+          animation: `${arcAnimation} 1000ms 1 normal forwards ease-in-out`,
+        }}
       />
-      <circle
-        cx={posX}
-        cy={posY}
-        r={radius}
-        fill={getFrontEleFill}
-        stroke={phase.includes('Cres') ? colors.black : undefined}
-        clipPath={useMask ? undefined : `url(#${phase})`}
-        mask={useMask ? `url(#${phase})` : undefined}
-        visibility={shouldHideFront}
+      <path
+        d={frontMoonPath}
+        fill={frontMoonColor}
+        filter="url(#moonShadow)"
+        style={{
+          offsetPath: `path('${arcPath}')`,
+          animation: `${arcAnimation} 1000ms 1 normal forwards ease-in-out`,
+        }}
       />
     </g>
   );
 };
 
-const MoonPhase = ({ posX, posY, radius, phase }) => (
+const MoonPhase = ({ posX, posY, radius, phase, arcPath, arcAnimation }) => (
   <g>
     <defs>
       <filter id="moonShadow" x="-100%" y="-100%" width="300%" height="300%">
@@ -51,26 +144,8 @@ const MoonPhase = ({ posX, posY, radius, phase }) => (
           <feMergeNode in="SourceGraphic" />
         </feMerge>
       </filter>
-      <mask id="waxCres">
-        <circle cx={posX - 5} cy={posY} r={radius} fill={colors.white} />
-      </mask>
-      <clipPath id="firstQuart" clipPathUnits="objectBoundingBox">
-        <rect x="0.5" y="0" width="0.5" height="1" />
-      </clipPath>
-      <mask id="waxGib">
-        <circle cx={posX + 7} cy={posY} r={radius} fill={colors.white} />
-      </mask>
-      <mask id="wanGib">
-        <circle cx={posX - 7} cy={posY} r={radius} fill={colors.white} />
-      </mask>
-      <clipPath id="thirdQuart" clipPathUnits="objectBoundingBox">
-        <rect x="0" y="0" width="0.5" height="1" />
-      </clipPath>
-      <mask id="wanCres">
-        <circle cx={posX + 5} cy={posY} r={radius} fill={colors.white} />
-      </mask>
     </defs>
-    {iconRender(posX, posY, radius, phase)}
+    {iconRender(posX, posY, radius, phase, arcPath, arcAnimation)}
   </g>
 );
 
@@ -79,6 +154,8 @@ MoonPhase.propTypes = {
   posY: PropTypes.string.isRequired,
   radius: PropTypes.string.isRequired,
   phase: PropTypes.string.isRequired,
+  arcPath: PropTypes.string.isRequired,
+  arcAnimation: PropTypes.string.isRequired,
 };
 
 export default MoonPhase;
