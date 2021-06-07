6933a1a47ebbab1328290cf89b516b25d165c868
Ben Bazyk <bbazyk@mojotech.com>
2021-06-07 14:10:44 -0400

    add active admin, remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # rubocop:disable Style/IfUnlessModifier
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
