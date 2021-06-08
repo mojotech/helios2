4cd05f8e982abab0c6c17a3d5e96ee8ece0c23dd
Ben Bazyk <bbazyk@mojotech.com>
2021-06-08 15:38:04 -0400

    changed dashboard info panel

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,32 +2,15 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin for Helios"
+          para "Use the above tabs to navigate models and edit their parameters"
+          para "Alter the displayed wifi and bathroom codes, rearrange and
+          \ adjust widget screentime, or even delete a traffic cam from view"
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end
-  # content
 end
