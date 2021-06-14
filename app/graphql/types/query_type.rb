c7634b97deff0e8c5621da92749ad55f454eb2fb
tucker-weed <tuckerjweed@gmail.com>
2021-06-14 14:31:12 -0400

    [fix] location response rails caching error fix

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -16,7 +16,7 @@ class Types::QueryType < Types::BaseObject
 
   def location(options = {})
     city_name = options[:city_name]
-    Rails.cache.fetch('location-response', expires_in: 15.minutes) do
+    Rails.cache.fetch(`location-response-#{city_name}`, expires_in: 15.minutes) do
       Location.where(city_name: city_name).first
     end
   end
