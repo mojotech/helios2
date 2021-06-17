d64fdd31836caf80076451cabaf7f86ea1c02dd7
Toby <tosa5156@colorado.edu>
2021-06-17 15:41:05 -0600

    feat: client receives deployed sha value via subscriptions and performs comparison with current sha for refresh

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -14,6 +14,7 @@ import { getMainDefinition } from 'apollo-utilities';
 import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
 import { persistCache } from 'apollo-cache-persist';
 import WidgetController from '@components/widget-controller';
+import versionCompare from '@components/version-compare';
 import helioSchema from '@javascript/schema.json';
 import GlobalStyle from '../styles';
 
@@ -72,6 +73,8 @@ const client = new ApolloClient({
     },
   },
 });
+
+versionCompare(client);
 
 const App = () => (
   <ApolloProvider client={client}>
