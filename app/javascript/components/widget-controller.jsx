c0ba5b21b90fe8306f793a8e403b12ccaa8d097c
tucker-weed <tuckerjweed@gmail.com>
2021-07-01 08:55:16 -0400

    [chore] WidgetController refactor to functional composition

--- app/javascript/components/widget-controller.jsx
+++ app/javascript/components/widget-controller.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import { mathMod } from 'ramda';
 import { Query } from 'react-apollo';
 import {
   WidgetDisplay,
   LoadingDisplay,
   DisconnectedDisplay,
 } from '@components/widget-display';
+import WidgetTransitionControls from '@components/widget-transition-controls';
 
 const getWidgets = gql`
   query getWidgets($id: Int!, $cityName: String!) {
@@ -30,178 +30,59 @@ const getWidgets = gql`
   }
 `;
 
-export class WidgetController extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentWidgetId: 0,
-      transitionTime: null,
-    };
-  }
+export const WidgetController = ({ client, cityName }) => {
+  const [newWidgetId, setNewWidgetId] = useState(0);
 
-  componentDidMount() {
-    const prefetchDelay = 1500;
+  const fetchPolicy = newWidgetId === 0 ? 'network-only' : 'cache-first';
 
-    this.lastPrefetchTime = 0;
-    this.timerId = setInterval(() => {
-      const { transitionTime, pausedTime } = this.state;
-
-      if (!transitionTime || pausedTime) {
-        return;
-      }
-
-      const { client } = this.props;
-      const prefetchTime = transitionTime - prefetchDelay;
-      const now = Date.now();
-
-      if (transitionTime < now) {
-        this.nextWidget();
-      } else if (
-        prefetchTime < now &&
-        this.lastPrefetchTime < now - prefetchDelay
-      ) {
-        const { nextWidgetId } = this.state;
-        const { cityName } = this.props;
-        this.lastPrefetchTime = now;
-        client.query({
-          query: getWidgets,
-          variables: {
-            id: nextWidgetId,
-            cityName,
-          },
-        });
-      }
-    }, 500);
-  }
-
-  componentWillUnmount() {
-    if (this.timerId) {
-      clearInterval(this.timerId);
-    }
-  }
-
-  keyToWidgetAction = ({ keyCode }) => {
-    const upKey = 38;
-    const downKey = 40;
-    const SPACEBAR = 32;
-    if (keyCode === SPACEBAR) {
-      this.setState(({ pausedTime, transitionTime }) =>
-        pausedTime
-          ? {
-              pausedTime: null,
-              transitionTime: transitionTime - pausedTime + Date.now(),
-            }
-          : { pausedTime: Date.now() },
-      );
-    }
-    if (keyCode === downKey) {
-      this.nextWidget();
-    }
-    if (keyCode === upKey) {
-      this.prevWidget();
-    }
-  };
-
-  nextWidget = () => {
-    this.setState(({ nextWidgetId }) =>
-      nextWidgetId
-        ? {
-            currentWidgetId: nextWidgetId,
-            nextWidgetId: null,
-            prevWidgetId: null,
-            transitionTime: null,
-          }
-        : {},
-    );
-  };
-
-  prevWidget = () => {
-    this.setState(({ prevWidgetId }) =>
-      prevWidgetId
-        ? {
-            currentWidgetId: prevWidgetId,
-            nextWidgetId: null,
-            prevWidgetId: null,
-            transitionTime: null,
-          }
-        : {},
-    );
-  };
-
-  switchToPage = id => {
-    this.setState({
-      currentWidgetId: id,
-      nextWidgetId: null,
-      prevWidgetId: null,
-      transitionTime: null,
+  const queryCallback = nextWidgetId => {
+    client.query({
+      query: getWidgets,
+      variables: {
+        id: nextWidgetId,
+        cityName,
+      },
     });
   };
 
-  clickToWidgetAction = ({ target }) => {
-    const targetId = target.id;
-    if (targetId.startsWith('widget_')) {
-      const widgetId = parseInt(targetId.replace('widget_', ''), 10);
-      this.switchToPage(widgetId);
-    }
-  };
+  return (
+    <Query
+      query={getWidgets}
+      variables={{ id: newWidgetId, cityName }}
+      fetchPolicy={fetchPolicy}
+      onCompleted={response => {
+        const {
+          location: {
+            widgets: { byIdOrFirst: current },
+          },
+        } = response;
+        setNewWidgetId(current.id);
+      }}
+    >
+      {({ loading, error, data }) => {
+        if (loading) {
+          return <LoadingDisplay cityName={cityName} />;
+        }
 
-  render() {
-    const { currentWidgetId, pausedTime } = this.state;
+        if (error) {
+          // eslint-disable-next-line
+          console.error(error);
+          return <DisconnectedDisplay cityName={cityName} />;
+        }
 
-    const fetchPolicy = currentWidgetId === 0 ? 'network-only' : 'cache-first';
-
-    const { cityName } = this.props;
-
-    return (
-      <Query
-        query={getWidgets}
-        variables={{ id: currentWidgetId, cityName }}
-        fetchPolicy={fetchPolicy}
-        onCompleted={response => {
-          const { location } = response;
-          const { widgets } = location;
-          const { enabled: enabledWidgets, byIdOrFirst: current } = widgets;
-
-          const index = enabledWidgets.findIndex(w => w.id === current.id);
-
-          const prevWidgetId =
-            enabledWidgets[mathMod(index - 1, enabledWidgets.length)].id;
-          const nextWidgetId =
-            enabledWidgets[mathMod(index + 1, enabledWidgets.length)].id;
-
-          this.setState({
-            currentWidgetId: current.id,
-            nextWidgetId,
-            prevWidgetId,
-            transitionTime: Date.now() + current.durationSeconds * 1000,
-          });
-        }}
-      >
-        {({ loading, error, data }) => {
-          if (loading) {
-            return <LoadingDisplay cityName={cityName} />;
-          }
-
-          if (error) {
-            // eslint-disable-next-line
-            console.error(error);
-            return <DisconnectedDisplay cityName={cityName} />;
-          }
-
-          return (
-            <WidgetDisplay
-              keyToWidgetAction={this.keyToWidgetAction}
-              clickToWidgetAction={this.clickToWidgetAction}
-              isPaused={!!pausedTime}
-              cityName={cityName}
-              widgets={data.location.widgets}
-            />
-          );
-        }}
-      </Query>
-    );
-  }
-}
+        return (
+          <WidgetTransitionControls
+            WrappedComponent={WidgetDisplay}
+            requestPrefetch={queryCallback}
+            requestWidget={setNewWidgetId}
+            cityName={cityName}
+            widgets={data.location.widgets}
+          />
+        );
+      }}
+    </Query>
+  );
+};
 
 WidgetController.propTypes = {
   client: PropTypes.shape({
---
+++ app/javascript/components/widget-transition-controls.jsx
@@ -0,0 +1,166 @@
+import React, { useEffect, useReducer } from 'react';
+import PropTypes from 'prop-types';
+import { mathMod } from 'ramda';
+
+const prefetchDelay = 1500;
+
+export const selectShouldRequestWidget = state =>
+  state.msRemainingSinceLastStart < Date.now() - state.startTimestamp;
+
+export const selectShouldRequestPrefetch = state =>
+  !state.prefetchRequestedYet &&
+  state.msRemainingSinceLastStart - (Date.now() - state.startTimestamp) <
+    prefetchDelay;
+
+export const selectIsPaused = state => state.startTimestamp === null;
+
+const resolveWidgetId = (enabledWidgets, currentWidgetId, { type }) => {
+  const index = enabledWidgets.findIndex(w => w.id === currentWidgetId);
+  switch (type) {
+    case 'nextWidget':
+      return enabledWidgets[mathMod(index + 1, enabledWidgets.length)].id;
+    case 'prevWidget':
+      return enabledWidgets[mathMod(index - 1, enabledWidgets.length)].id;
+    default:
+      return null;
+  }
+};
+
+const reducer = (state, action) => {
+  const { startTimestamp, msRemainingSinceLastStart } = state;
+  const now = Date.now();
+
+  switch (action.type) {
+    case 'togglePause':
+      return {
+        ...state,
+        msRemainingSinceLastStart: selectIsPaused(state)
+          ? msRemainingSinceLastStart
+          : msRemainingSinceLastStart - (now - startTimestamp),
+        startTimestamp: selectIsPaused(state) ? now : null,
+      };
+    case 'startTimer':
+      return {
+        msRemainingSinceLastStart: action.msDurationSeconds,
+        startTimestamp: selectIsPaused(state) ? null : now,
+        prefetchRequestedYet: false,
+      };
+    case 'tryPrefetch':
+      if (selectShouldRequestPrefetch(state)) {
+        action.prefetch();
+        return {
+          ...state,
+          prefetchRequestedYet: true,
+        };
+      }
+      return state;
+    default:
+      return state;
+  }
+};
+
+const WidgetTransitionControls = ({
+  WrappedComponent,
+  requestPrefetch,
+  requestWidget,
+  cityName,
+  widgets,
+}) => {
+  const { byIdOrFirst: current, enabled: enabledWidgets } = widgets;
+
+  const widgetTransitionTime = current.durationSeconds * 1000;
+
+  const [state, dispatch] = useReducer(reducer, {
+    startTimestamp: Date.now(),
+    msRemainingSinceLastStart: null,
+    prefetchRequestedYet: false,
+  });
+
+  useEffect(() => {
+    dispatch({
+      type: 'startTimer',
+      msDurationSeconds: widgetTransitionTime,
+    });
+  }, [current.id]);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (selectIsPaused(state)) {
+        return;
+      }
+
+      const nextWidgetId = resolveWidgetId(enabledWidgets, current.id, {
+        type: 'nextWidget',
+      });
+
+      dispatch({
+        type: 'tryPrefetch',
+        prefetch: () => requestPrefetch(nextWidgetId),
+      });
+
+      if (selectShouldRequestWidget(state)) {
+        requestWidget(nextWidgetId);
+      }
+    }, 250);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [state]);
+
+  const keyToWidgetAction = ({ keyCode }) => {
+    const upKey = 38;
+    const downKey = 40;
+    const SPACEBAR = 32;
+    if (keyCode === SPACEBAR) {
+      dispatch({ type: 'togglePause' });
+    }
+    if (keyCode === downKey) {
+      requestWidget(
+        resolveWidgetId(enabledWidgets, current.id, { type: 'nextWidget' }),
+      );
+    }
+    if (keyCode === upKey) {
+      requestWidget(
+        resolveWidgetId(enabledWidgets, current.id, { type: 'prevWidget' }),
+      );
+    }
+  };
+
+  const clickToWidgetAction = ({ target }) => {
+    const targetId = target.id;
+    if (targetId.startsWith('widget_')) {
+      const widgetId = parseInt(targetId.replace('widget_', ''), 10);
+      requestWidget(widgetId);
+    }
+  };
+
+  return (
+    <WrappedComponent
+      keyToWidgetAction={keyToWidgetAction}
+      clickToWidgetAction={clickToWidgetAction}
+      isPaused={selectIsPaused(state)}
+      cityName={cityName}
+      widgets={widgets}
+    />
+  );
+};
+
+WidgetTransitionControls.propTypes = {
+  WrappedComponent: PropTypes.func.isRequired,
+  requestPrefetch: PropTypes.func.isRequired,
+  requestWidget: PropTypes.func.isRequired,
+  cityName: PropTypes.string.isRequired,
+  widgets: PropTypes.shape({
+    enabled: PropTypes.array.isRequired,
+    byIdOrFirst: PropTypes.shape({
+      durationSeconds: PropTypes.number.isRequired,
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      showWeather: PropTypes.bool.isRequired,
+      sidebarText: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default WidgetTransitionControls;
