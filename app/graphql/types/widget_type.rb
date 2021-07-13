7350c82fe2fb21e6599f8ed59222a5b5f340dfaf
Fallyn Logan <fallynlogan99@gmail.com>
2021-07-13 15:25:44 -0600

    refactor resolve_type to pass rubocop tests

--- app/graphql/types/widget_type.rb
+++ app/graphql/types/widget_type.rb
@@ -14,15 +14,11 @@ module Types::WidgetType
 
   definition_methods do
     def resolve_type(obj, _ctx)
-      case obj.name
-      when 'Guests' then Types::GuestsWidget
-      when 'Weather' then Types::WeatherWidget
-      when 'Twitter' then Types::TwitterWidget
-      when 'Numbers' then Types::NumbersWidget
-      when 'Traffic' then Types::TrafficWidget
-      when 'Events' then Types::EventsWidget
-      else raise "Unexpected WidgetType: #{obj.inspect}"
-      end
+      widgets = { 'Guests' => Types::GuestsWidget, 'Weather' => Types::WeatherWidget,
+                  'Twitter' => Types::TwitterWidget, 'Numbers' => Types::NumbersWidget,
+                  'Traffic' => Types::TrafficWidget, 'Events' => Types::EventsWidget }
+      raise "Unexpected WidgetType: #{obj.inspect}" unless widgets[obj.name]
+      widgets[obj.name]
     end
   end
 end
