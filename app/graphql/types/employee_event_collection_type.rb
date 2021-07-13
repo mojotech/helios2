b5fb0908347f37ce73fa345206a71aa0d1c7c30f
Fallyn Logan <fallynlogan99@gmail.com>
2021-07-13 13:16:04 -0600

    Feat: add employees graphql endpoint

---
+++ app/graphql/types/employee_event_collection_type.rb
@@ -0,0 +1,17 @@
+class Types::EmployeeEventCollectionType < Types::BaseObject
+  field :birthday_employees, [Types::EmployeeType]
+  field :anniversary_employees, [Types::EmployeeType]
+  field :time_off_employees, [Types::EmployeeType]
+
+  def birthday_employees
+    Clients::BamboohrClient.find_employees.find_birthday_employees
+  end
+
+  def anniversary_employees
+    Clients::BamboohrClient.find_employees.find_anniversary_employees
+  end
+
+  def time_off_employees
+    Clients::BamboohrClient.find_employees.find_time_off_employees
+  end
+end
