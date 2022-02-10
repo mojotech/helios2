8b3dcbfebfea8748becf4d8812c16647fad7151b
malssid <mlalssid@gmail.com>
2022-02-10 11:47:28 -0500

    chore: disable database temporarily

--- server-phoenix/lib/helios/application.ex
+++ server-phoenix/lib/helios/application.ex
@@ -9,7 +9,7 @@ defmodule Helios.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      Helios.Repo,
+      # Helios.Repo,
       # Start the Telemetry supervisor
       HeliosWeb.Telemetry,
       # Start the PubSub system
