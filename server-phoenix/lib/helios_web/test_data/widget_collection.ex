9c544c5ccec3408564f1bebcd40d685419fb8d24
malssid <mlalssid@gmail.com>
2022-03-03 11:36:38 -0500

    feat: add traffic cam resolver and widget

--- server-phoenix/lib/helios_web/test_data/widget_collection.ex
+++ server-phoenix/lib/helios_web/test_data/widget_collection.ex
@@ -1,5 +1,5 @@
 defmodule HeliosWeb.TestData.WidgetCollection do
   def enabled do
-    "[{\"id\":2,\"name\":\"Weather\",\"enabled\":true,\"duration_seconds\":20,\"position\":1,\"created_at\":\"2022-02-22T19:48:40.759Z\",\"updated_at\":\"2022-02-22T19:48:40.759Z\",\"location_id\":1,\"start\":null,\"stop\":null,\"sidebar_text\":\"Weather\",\"show_weather\":false}]"
+    "[{\"id\":2,\"name\":\"Weather\",\"enabled\":true,\"duration_seconds\":20,\"position\":1,\"created_at\":\"2022-02-22T19:48:40.759Z\",\"updated_at\":\"2022-02-22T19:48:40.759Z\",\"location_id\":1,\"start\":null,\"stop\":null,\"sidebar_text\":\"Weather\",\"show_weather\":false}, {\"id\":5,\"name\":\"Traffic\",\"enabled\":true,\"duration_seconds\":20,\"position\":4,\"created_at\":\"2022-02-28T20:08:27.394Z\",\"updated_at\":\"2022-02-28T20:08:27.394Z\",\"location_id\":1,\"start\":{\"hour\":0,\"minute\":0,\"second\":0,\"second_of_day\":57600},\"stop\":{\"hour\":0,\"minute\":0,\"second\":0,\"second_of_day\":82800},\"sidebar_text\":\"Traffic\",\"show_weather\":true}]"
   end
 end
