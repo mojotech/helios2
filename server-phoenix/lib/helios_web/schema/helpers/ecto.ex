11796f97ef47472ad8c7b4eaed057dc7de58e4d4
malssid <mlalssid@gmail.com>
2022-03-14 14:40:19 -0400

    feat: define find_or_initialize_by alternative for elixir

---
+++ server-phoenix/lib/helios_web/schema/helpers/ecto.ex
@@ -0,0 +1,16 @@
+defmodule HeliosWeb.Schema.Helpers.Ecto do
+  alias Helios.Repo
+
+  def find_or_initialize_by(model, params \\ [], data) do
+    case Repo.get_by(model, params) do
+      nil ->
+        params = Enum.into(params, %{})
+        data = Map.merge(data, params)
+        response = Repo.insert!(data)
+        response
+
+      result ->
+        result
+    end
+  end
+end
