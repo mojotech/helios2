09617544661566ea765d7c18fde19b52547291bc
linalim <seohyunlim98@gmail.com>
2022-03-18 13:40:50 -0400

    feat: traffic cam resolver added

--- server-phoenix/lib/helios/traffic_cam.ex
+++ server-phoenix/lib/helios/traffic_cam.ex
@@ -1,6 +1,7 @@
 defmodule Helios.TrafficCam do
   use Ecto.Schema
   alias Helios.Location
+  import Ecto.Query
 
   @primary_key {:id, :id, autogenerate: true}
   schema "traffic_cams" do
@@ -10,5 +11,9 @@ defmodule Helios.TrafficCam do
     timestamps(inserted_at: :created_at, type: :utc_datetime)
 
     belongs_to :location, Location
+  end
+
+  def with_location(query, location) do
+    from q in query, where: q.location_id == ^location.id
   end
 end
