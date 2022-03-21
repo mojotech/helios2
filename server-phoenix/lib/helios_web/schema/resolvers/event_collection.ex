9cdc24e813264fb48d5f4d61bec7b41f3d78c6dd
malssid <mlalssid@gmail.com>
2022-03-21 16:59:16 -0400

    feat: configure events resolver to use database

--- server-phoenix/lib/helios_web/schema/resolvers/event_collection.ex
+++ server-phoenix/lib/helios_web/schema/resolvers/event_collection.ex
@@ -1,45 +1,30 @@
 defmodule HeliosWeb.Schema.Resolvers.EventCollection do
-  @test_query_data %{
-    "all" => [
-      %{"id" => 1, "source" => "github_pull", "created_at" => ~U[2022-02-16 16:31:17Z]},
-      %{"id" => 2, "source" => "github_commit", "created_at" => ~U[2022-02-16 16:33:17Z]},
-      %{"id" => 3, "source" => "slack_message", "created_at" => ~U[2022-02-16 16:37:17Z]},
-      %{"id" => 4, "source" => "github_commit", "created_at" => ~U[2022-02-17 16:31:17Z]},
-      %{"id" => 5, "source" => "github_commit", "created_at" => ~U[2022-02-17 16:33:17Z]},
-      %{"id" => 6, "source" => "slack_message", "created_at" => ~U[2022-02-17 16:35:17Z]}
-    ],
-    "count" => %{"github_pull" => 1, "github_commit" => 3, "slack_message" => 2}
-  }
-
-  defp test_query(args) do
-    new_data =
-      case args do
-        %{type: type} ->
-          @test_query_data["all"]
-          |> Enum.filter(fn event -> event["source"] == type end)
-
-        %{created_after: created_after} ->
-          @test_query_data["all"]
-          |> Enum.filter(fn event ->
-            DateTime.compare(event["created_at"], elem(DateTime.from_iso8601(created_after), 1)) ==
-              :gt
-          end)
-
-        %{created_after: created_after, type: type} ->
-          @test_query_data["all"]
-          |> Enum.filter(fn event ->
-            DateTime.compare(event["created_at"], elem(DateTime.from_iso8601(created_after), 1)) ==
-              :gt and event["source"] == type
-          end)
-
-        _ ->
-          @test_query_data["all"]
-      end
-
-    %{"all" => new_data, "count" => @test_query_data["count"]}
-  end
+  alias Helios.{Repo, Event}
 
   def events(_parent, args, _info) do
-    {:ok, test_query(args)}
+    event_query =
+      case args do
+        %{type: type} ->
+          Event
+          |> Event.with_source(type)
+
+        %{created_after: created_after} ->
+          Event
+          |> Event.created_after(created_after)
+
+        %{created_after: created_after, type: type} ->
+          Event
+          |> Event.with_source(type)
+          |> Event.created_after(created_after)
+
+        %{} ->
+          Event
+      end
+
+    {:ok,
+     %{
+       all: Repo.all(event_query),
+       count: Event.count(Event) |> Repo.all() |> Enum.into(%{})
+     }}
   end
 end
