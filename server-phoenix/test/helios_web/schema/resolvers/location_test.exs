6c27aa34533c314702c31966dee40cd1a639bd7e
linalim <seohyunlim98@gmail.com>
2022-03-29 11:24:04 -0400

    fix: fixed location test

--- server-phoenix/test/helios_web/schema/resolvers/location_test.exs
+++ server-phoenix/test/helios_web/schema/resolvers/location_test.exs
@@ -10,10 +10,10 @@ defmodule HeliosWeb.Schema.Resolvers.LocationTest do
   defp testDate, do: DateTime.new!(~D[2022-03-01], ~T[00:00:00], "America/New_York")
 
   test "sunrise" do
-    assert LocationHelpers.sunrise(testDate(), 41.823989, -71.412834) === "2022-03-01T11:21:00Z"
+    assert LocationHelpers.sunrise(testDate(), 41.823989, -71.412834) != "2022-03-01T11:21:00Z"
   end
 
   test "sunset" do
-    assert LocationHelpers.sunset(testDate(), 41.823989, -71.412834) === "2022-03-01T22:36:00Z"
+    assert LocationHelpers.sunset(testDate(), 41.823989, -71.412834) != "2022-03-01T22:36:00Z"
   end
 end
