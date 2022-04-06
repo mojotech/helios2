547f6016101ea989cc2c62f65e45eaaadc6538df
malssid <mlalssid@gmail.com>
2022-04-06 15:25:42 -0400

    feat: enable sqlite for phoenix tests

--- server-phoenix/config/test.exs
+++ server-phoenix/config/test.exs
@@ -8,13 +8,18 @@ config :bcrypt_elixir, :log_rounds, 1
 # The MIX_TEST_PARTITION environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
+
 config :helios, Helios.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "helios_test#{System.get_env("MIX_TEST_PARTITION")}",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  database: "../db/test.sqlite3",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# config :helios, Helios.Repo,
+#   username: "postgres",
+#   password: "postgres",
+#   hostname: "localhost",
+#   database: "helios_test#{System.get_env("MIX_TEST_PARTITION")}",
+#   pool: Ecto.Adapters.SQL.Sandbox,
+#   pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
