5d965facab111da65ebf21cb5ab1f0fab00af37c
Priscila Trevino <priscilatrevino@Priscilas-MacBook-Pro.local>
2022-05-31 16:01:30 -0600

    feat: adding anticipated precipitation description

--- app/javascript/components/widgets/weather/current-weather.jsx
+++ app/javascript/components/widgets/weather/current-weather.jsx
@@ -14,6 +14,9 @@ export const getCurrentWeather = gql`
       }
       windGust
     }
+    minutely {
+      precipitation
+    }
   }
 `;
 
@@ -21,6 +24,12 @@ const IconWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+const DescriptionWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
 `;
 
 const WeatherDescription = ({ baseDescription, weather }) => {
@@ -30,17 +39,54 @@ const WeatherDescription = ({ baseDescription, weather }) => {
   return baseDescription;
 };
 
+export const AnticipatedPrecipitation = ({ weather }) => {
+  const precip = [];
+  for (let i = 0; i < weather.minutely.length; i += 1) {
+    if (weather.minutely[i].precipitation !== 0) {
+      precip.push({ minute: i, value: weather.minutely[i].precipitation });
+    }
+  }
+  if (precip.length === 0) {
+    return '';
+  }
+  const averagePrecipitation =
+    precip.reduce((acc, val) => acc + val.value, 0) / precip.length;
+  let descStart = `in ${precip[0].minute} minutes`;
+  let descEnd = `ending in ${precip[precip.length - 1].minute} minutes.`;
+  if (precip[0].minute === 0) {
+    descStart = `now`;
+  }
+  if (precip[precip.length - 1].minute === 60) {
+    descEnd = `continuing past the hour.`;
+  }
+  switch (true) {
+    case averagePrecipitation > 50.0:
+      return `Violent Precipitation starting ${descStart} and ${descEnd}`;
+    case averagePrecipitation > 7.5:
+      return `Heavy Precipitation starting ${descStart} and ${descEnd}`;
+    case averagePrecipitation > 2.5:
+      return `Moderate Precipitation starting ${descStart} and ${descEnd}`;
+    default:
+      return `Light Precipitation starting ${descStart} and ${descEnd}`;
+  }
+};
+
 const CurrentWeather = ({ weather, useLargeIcon }) => {
   const { description, icon } = weather.current.weather;
   return (
-    <IconWrapper>
-      {useLargeIcon ? (
-        <LargeSkyIcon icon={icon} />
-      ) : (
-        <SmallSkyIcon icon={icon} />
-      )}
-      <WeatherDescription baseDescription={description} weather={weather} />
-    </IconWrapper>
+    <div>
+      <IconWrapper>
+        {useLargeIcon ? (
+          <LargeSkyIcon icon={icon} />
+        ) : (
+          <SmallSkyIcon icon={icon} />
+        )}
+        <WeatherDescription baseDescription={description} weather={weather} />
+      </IconWrapper>
+      <DescriptionWrapper>
+        <AnticipatedPrecipitation weather={weather} />
+      </DescriptionWrapper>
+    </div>
   );
 };
 
