f70834e3b6d01304c9b816ed0193c28d38a1ca5d
LeoSipowicz <leojsipo@gmail.com>
2022-06-03 11:31:50 -0600

    chore: remove variable expansion ambiguity

--- server-phoenix/lib/helios_web/AbsintheChannelDecorator.ex
+++ server-phoenix/lib/helios_web/AbsintheChannelDecorator.ex
@@ -43,7 +43,7 @@ defmodule HeliosWeb.AbsintheChannelDecorator do
           ]
 
           opts = Keyword.put(config_opts, :variables, variables)
-          context = socket.assigns.absinthe.opts[:context]
+          # context = socket.assigns.absinthe.opts[:context]
           run(query, config[:schema], config[:pipeline], opts)
         end
 
