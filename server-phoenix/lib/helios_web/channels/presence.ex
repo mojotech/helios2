2c2217bb876c774caf460ec56954955d69e260ef
LeoSipowicz <leojsipo@gmail.com>
2022-06-06 13:40:36 -0600

    presence fix squash with generate presence.ex to clear Presence is undefined warning

--- server-phoenix/lib/helios_web/channels/presence.ex
+++ server-phoenix/lib/helios_web/channels/presence.ex
@@ -5,12 +5,7 @@ defmodule HeliosWeb.Presence do
   See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
   docs for more details.
   """
-<<<<<<< HEAD
   use Phoenix.Presence,
     otp_app: :helios,
     pubsub_server: Helios.PubSub
-=======
-  use Phoenix.Presence, otp_app: :helios,
-                        pubsub_server: Helios.PubSub
->>>>>>> 3e9fa9aed7669fdd76ab787007a8b053707b8d7b
 end
