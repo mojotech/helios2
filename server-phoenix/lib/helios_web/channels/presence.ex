bb62e6b0fdb53559f912b91c188bf1b6786c8061
LeoSipowicz <leojsipo@gmail.com>
2022-06-06 16:00:20 -0600

    chore: generate presence.ex to clear Presence is undefined warning

---
+++ server-phoenix/lib/helios_web/channels/presence.ex
@@ -0,0 +1,11 @@
+defmodule HeliosWeb.Presence do
+  @moduledoc """
+  Provides presence tracking to channels and processes.
+
+  See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
+  docs for more details.
+  """
+  use Phoenix.Presence,
+    otp_app: :helios,
+    pubsub_server: Helios.PubSub
+end
