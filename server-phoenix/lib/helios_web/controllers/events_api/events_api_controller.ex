35eaabfd32d5759bf871f55ded9fd618e5dd9ec8
RVRX <colemanning@yandex.com>
2022-06-20 13:45:20 -0400

    feat: add REST API for adding commits

---
+++ server-phoenix/lib/helios_web/controllers/events_api/events_api_controller.ex
@@ -0,0 +1,54 @@
+defmodule HeliosWeb.EventsAPI.EventsAPIController do
+  use HeliosWeb, :controller
+  alias Helios.{Repo, Event}
+
+  def handle(conn, params) do
+    cond do
+      params["version"] == "1" -> api_v1(conn, params)
+      true -> send_resp(conn, 404, "Not Found")
+    end
+  end
+
+  defp api_v1(conn, params) do
+    case params["event_type"] do
+      "github_commit" ->
+        post_event(
+          conn,
+          Event.commits(Event),
+          :github_commit,
+          params["event_id"],
+          params["author"]
+        )
+
+      _ ->
+        send_resp(conn, 400, "Bad Request")
+    end
+  end
+
+  defp post_event(conn, source_type_query, source, external_id, source_author) do
+    unless(
+      source_type_query
+      |> Event.with_external_id(external_id)
+      |> Repo.exists?()
+    ) do
+      Repo.insert!(%Event{
+        source: source,
+        external_id: external_id,
+        source_author: source_author
+      })
+      |> publish
+
+      send_resp(conn, 201, "Created")
+    else
+      send_resp(conn, 208, "Already Reported")
+    end
+  end
+
+  defp publish(event) do
+    Absinthe.Subscription.publish(
+      HeliosWeb.Endpoint,
+      event,
+      event_published: "all"
+    )
+  end
+end
