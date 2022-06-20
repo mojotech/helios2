a3219f856c6c56ed36ca9f2c61d173380a5effc6
RVRX <colemanning@yandex.com>
2022-06-20 14:17:29 -0400

    feat: Add PR support to Events API

--- server-phoenix/test/helios_web/controllers/events_api/events_api_controller_test.exs
+++ server-phoenix/test/helios_web/controllers/events_api/events_api_controller_test.exs
@@ -80,9 +80,80 @@ defmodule HeliosWeb.WebHooks.EventsAPIControllerTest do
     assert length(get_commits()) == 1
   end
 
+  test "POST new PR", %{conn: conn} do
+    conn =
+      conn
+      |> put_req_header("content-type", "application/json")
+      |> post(
+        Routes.events_api_path(conn, :handle, "1"),
+        %{
+          event_type: "github_pr",
+          author: "RVRX",
+          event_id: "726abde67d8f5eaf1f6ba5c57fc3c7d91ac0fd1c"
+        }
+      )
+
+    assert conn.status == 201
+    assert length(get_commits()) == 0
+    assert length(get_prs()) == 1
+  end
+
+  test "POST multiple commits (w/ duplicate IDs) and PRs", %{conn: conn} do
+    conn = conn |> put_req_header("content-type", "application/json")
+
+    conn
+    |> post(
+      Routes.events_api_path(conn, :handle, "1"),
+      %{
+        event_type: "github_commit",
+        author: "some1",
+        event_id: "a-common-hash"
+      }
+    )
+
+    conn
+    |> post(
+      Routes.events_api_path(conn, :handle, "1"),
+      %{
+        event_type: "github_commit",
+        author: "some1 else w/ same id",
+        event_id: "a-common-hash"
+      }
+    )
+
+    conn
+    |> post(
+      Routes.events_api_path(conn, :handle, "1"),
+      %{
+        event_type: "github_pr",
+        author: "RVRX",
+        event_id: "unique-hash"
+      }
+    )
+
+    conn
+    |> post(
+      Routes.events_api_path(conn, :handle, "1"),
+      %{
+        event_type: "github_pr",
+        author: "RVRX",
+        event_id: "another-unique-hash"
+      }
+    )
+
+    assert length(get_commits()) == 1
+    assert length(get_prs()) == 2
+  end
+
   defp get_commits() do
     Event
     |> Event.commits()
+    |> Repo.all()
+  end
+
+  defp get_prs() do
+    Event
+    |> Event.pull_requests()
     |> Repo.all()
   end
 end
