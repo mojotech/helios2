73bb2dab8c8252a528a814aca91ccacee6d61e61
wogara <weogara@gmail.com>
2022-06-29 10:42:57 -0400

    [feature] collect the slack channel names and store in names/ids table

--- server-phoenix/lib/helios_web/controllers/web_hooks/slack_controller.ex
+++ server-phoenix/lib/helios_web/controllers/web_hooks/slack_controller.ex
@@ -1,9 +1,41 @@
 defmodule HeliosWeb.WebHooks.SlackController do
   use HeliosWeb, :controller
-  alias Helios.{Repo, Event}
+
+  alias Helios.{Repo, Event, SlackChannelNames}
 
   defp slack_bearer_token,
     do: Application.get_env(:helios, HeliosWeb.Endpoint)[:slack_bearer_token]
+
+  defp insert_channel_id(channel_id) do
+    unless SlackChannelNames
+           |> SlackChannelNames.with_id(channel_id)
+           |> Repo.exists?() do
+      body = %{
+        "channel" => channel_id
+      }
+
+      request_body = URI.encode_query(body)
+
+      headers = [
+        {"Accept", "application/json"},
+        {"Authorization", "Bearer #{slack_bearer_token()}"},
+        {"Content-Type", "application/x-www-form-urlencoded"}
+      ]
+
+      channel_response =
+        HTTPoison.post!("https://slack.com/api/conversations.info", request_body, headers,
+          follow_redirect: true
+        )
+
+      json = Jason.decode!(channel_response.body)
+      name = json["channel"]["name"]
+
+      Repo.insert!(%SlackChannelNames{
+        channel_id: channel_id,
+        channel_name: name
+      })
+    end
+  end
 
   def handle(conn, params) do
     if params["type"] == "url_verification" do
@@ -24,6 +56,10 @@ defmodule HeliosWeb.WebHooks.SlackController do
           end
         )
       end
+
+      channel_id = params["event"]["channel"]
+
+      insert_channel_id(channel_id)
 
       unless Event
              |> Event.slack_messages()
