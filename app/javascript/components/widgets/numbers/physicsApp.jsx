fdb730095e47be440a4c82cd8013353fb77e1681
LeoSipowicz <leojsipo@gmail.com>
2022-08-09 14:09:48 -0600

    add: rapier ball physics and pixi.js renderer

---
+++ app/javascript/components/widgets/numbers/physicsApp.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import RAPIER from '@dimforge/rapier2d-compat';
+import * as PIXI from 'pixi.js';
+import runSimulation from './rapierPixi';
+
+export default function PhysicsApp(eventCount) {
+  const Appwidth = 1100;
+  const Appheight = 600;
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    const app = new PIXI.Application({
+      width: Appwidth,
+      height: Appheight,
+      view: ref.current,
+    });
+    RAPIER.init().then(() =>
+      runSimulation(app, eventCount, Appwidth, Appheight),
+    );
+  }, []);
+  return (
+    <div className="App">
+      <canvas ref={ref}>render into</canvas>
+    </div>
+  );
+}
